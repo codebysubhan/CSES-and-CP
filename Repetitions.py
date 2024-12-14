fa642e35e062b35ac5252cd7d9b9ec2ba076ae45
codebysubhan <bsdsf22m030@pucit.edu.pk>
2024-12-14 12:12:22 +0500

    Repititions solved

--- Repetitions.py
+++ Repetitions.py
@@ -1,9 +1,20 @@
 import sys
 line = str(sys.stdin.readline().strip())
-count = dict()
-for letter in line:
-    if letter not in count:
-        count[letter] = 1
-    else:
-        count[letter] += 1
-sys.stdout.write(str(max(count.values())))
+res = float("-inf")
+i = 0
+count = 0
+flag = line[0]
+while i < len(line):
+    while flag == line[i]:
+        count += 1
+        i += 1
+        if i >= len(line):
+            break
+    if res < count:
+        res = count
+    count = 1
+    if i >= len(line):
+        break
+    flag = line[i]
+    i += 1
+sys.stdout.write(str(res))
